e466303f42f5ad00093770c955b1c4cfaea25010
evgeny <khokhlov.e.n@gmail.com>
2022-12-01 11:44:20 +0300

    Part 1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,6 +1,18 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        val iterator = input.iterator()
+        val getNextElfCalories = nextElfCalories@{
+            if (!iterator.hasNext()) return@nextElfCalories null
+            sequence {
+                while (iterator.hasNext()) {
+                    val next = iterator.next()
+                    if (next.isEmpty()) return@sequence
+                    yield(next.toInt())
+                }
+            }
+        }
+        val elfCalories: Sequence<Sequence<Int>> = generateSequence(getNextElfCalories)
+        return elfCalories.map { it.sum() }.max()
     }
 
     fun part2(input: List<String>): Int {
@@ -9,7 +21,7 @@ fun main() {
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 24_000)
 
     val input = readInput("Day01")
     println(part1(input))
