ec4a1851026c1e75d473980ad0ffbcaee4af42d9
evgeny <khokhlov.e.n@gmail.com>
2022-12-01 19:36:28 +0300

    Clean up

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,41 +1,17 @@
 fun main() {
-    fun <T : Comparable<T>> Sequence<T>.topThree(): List<T> {
-        val topThreeValues = mutableListOf<T>()
-        val iterator = iterator()
-        while (iterator.hasNext()) {
-            val e = iterator.next()
-            if (topThreeValues.size < 3) {
-                topThreeValues.add(e)
-                topThreeValues.sort()
-            } else if (topThreeValues.first() < e) {
-                topThreeValues[0] = e
-                topThreeValues.sort()
-            }
-        }
-        return topThreeValues
-    }
 
-    fun inputToElfCalories(input: List<String>): Sequence<Int> {
-        val iterator = input.iterator()
-        val getNextElfCalories = nextElfCalories@{
-            if (!iterator.hasNext()) return@nextElfCalories null
-            sequence {
-                while (iterator.hasNext()) {
-                    val next = iterator.next()
-                    if (next.isEmpty()) return@sequence
-                    yield(next.toInt())
-                }
-            }
-        }
-        return generateSequence(getNextElfCalories).map { it.sum() }
+    fun getElfCalories(input: List<String>): Sequence<Int> {
+        return input.asSequence()
+            .splitBy { it.isEmpty() }
+            .map { it.map(String::toInt).sum() }
     }
 
     fun part1(input: List<String>): Int {
-        return inputToElfCalories(input).max()
+        return getElfCalories(input).max()
     }
 
     fun part2(input: List<String>): Int {
-        return inputToElfCalories(input).topThree().sum()
+        return getElfCalories(input).topThree().sum()
     }
 
     // test if implementation meets criteria from the description, like:
--- src/Utils.kt
+++ src/Utils.kt
@@ -14,3 +14,36 @@ fun readInput(name: String) = File("src", "$name.txt")
 fun String.md5() = BigInteger(1, MessageDigest.getInstance("MD5").digest(toByteArray()))
     .toString(16)
     .padStart(32, '0')
+
+
+fun <T> Sequence<T>.splitBy(predicate: (T) -> Boolean): Sequence<Sequence<T>> {
+    val iterator = this.iterator()
+    return generateSequence {
+        if (!iterator.hasNext()) return@generateSequence null
+        sequence {
+            while (iterator.hasNext()) {
+                val next = iterator.next()
+                if (predicate(next)) return@sequence
+                yield(next)
+            }
+        }
+    }
+}
+
+fun <T : Comparable<T>> Sequence<T>.top(n: Int): List<T> {
+    val topN = mutableListOf<T>()
+    val iterator = iterator()
+    while (iterator.hasNext()) {
+        val e = iterator.next()
+        if (topN.size < n) {
+            topN.add(e)
+            topN.sort()
+        } else if (topN.first() < e) {
+            topN[0] = e
+            topN.sort()
+        }
+    }
+    return topN
+}
+
+fun <T : Comparable<T>> Sequence<T>.topThree(): List<T> = this.top(3)
