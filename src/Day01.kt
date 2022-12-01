9a18bbf8eb5a52d4668441f555af620d556498d2
evgeny <khokhlov.e.n@gmail.com>
2022-12-01 12:04:54 +0300

    Part 2

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,5 +1,21 @@
 fun main() {
-    fun part1(input: List<String>): Int {
+    fun <T : Comparable<T>> Sequence<T>.topThree(): List<T> {
+        val topThreeValues = mutableListOf<T>()
+        val iterator = iterator()
+        while (iterator.hasNext()) {
+            val e = iterator.next()
+            if (topThreeValues.size < 3) {
+                topThreeValues.add(e)
+                topThreeValues.sort()
+            } else if (topThreeValues.first() < e) {
+                topThreeValues[0] = e
+                topThreeValues.sort()
+            }
+        }
+        return topThreeValues
+    }
+
+    fun inputToElfCalories(input: List<String>): Sequence<Int> {
         val iterator = input.iterator()
         val getNextElfCalories = nextElfCalories@{
             if (!iterator.hasNext()) return@nextElfCalories null
@@ -11,17 +27,21 @@ fun main() {
                 }
             }
         }
-        val elfCalories: Sequence<Sequence<Int>> = generateSequence(getNextElfCalories)
-        return elfCalories.map { it.sum() }.max()
+        return generateSequence(getNextElfCalories).map { it.sum() }
+    }
+
+    fun part1(input: List<String>): Int {
+        return inputToElfCalories(input).max()
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        return inputToElfCalories(input).topThree().sum()
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
     check(part1(testInput) == 24_000)
+    check(part2(testInput) == 45_000)
 
     val input = readInput("Day01")
     println(part1(input))
